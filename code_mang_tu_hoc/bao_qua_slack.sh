ca070cbff797decd1f11d00879249b5a7ca0e80c
namgivu <namgivu@gmail.com>
2020-12-08 17:09:00 +0700

    push

--- code_mang_tu_hoc/bao_qua_slack.sh
+++ code_mang_tu_hoc/bao_qua_slack.sh
@@ -1,5 +1,8 @@
 # https://www.cloudsavvyit.com/289/how-to-send-a-message-to-slack-from-a-bash-script/
+#!/bin/bash
 alert_via_slack(){
 	url="https://hooks.slack.com/services/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"
 curl -X POST -H 'Content-type: application/json' --data '{"text":"httpd is stop"}' ${url}
 }
+
+alert_via_slack
